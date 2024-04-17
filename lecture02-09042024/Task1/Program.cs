f93d342d84a4a275420f3c1559fbc2541da0b556
Sergey Bartasevich <sbartasevich@gmail.com>
2024-04-17 21:47:55 +0300

    modify-01 lectures/lecture02-09042024/Task1/Program.cs

--- lecture02-09042024/Task1/Program.cs
+++ lecture02-09042024/Task1/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] arr = new int[5];
+arr[0] = 15;
+arr[1] = 3;
+arr[2] = 7;
+arr[3] = 5;
+arr[4] = 10;
+Console.WriteLine(arr[0]);
+Console.WriteLine(arr[1]);
+Console.WriteLine(arr[2]);
+Console.WriteLine(arr[3]);
+Console.WriteLine(arr[4]);
+
+// Console.WriteLine(arr[5]);
+
+int[] arr2 = new int[5] { 12, 2, 3, 5, 89 };
+
+int[] arr3 = { 34, 2, 6, 17, 0 };
