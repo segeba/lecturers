036a1b1b1a9930696a2a19e8d2e4d6a57c694be5
Sergey Bartasevich <sbartasevich@gmail.com>
2024-04-17 21:33:57 +0300

    first commit

---
+++ lecture01-02042024/Task1/Program.cs
@@ -0,0 +1,9 @@
+﻿int value = 100;
+double d = 12.33;
+char c = '?';
+bool b = true;
+
+Console.WriteLine(value);
+Console.WriteLine(d);
+Console.WriteLine(c);
+Console.WriteLine(b);
---
+++ lecture01-02042024/Task2/Program.cs
@@ -0,0 +1,30 @@
+﻿int i1 = 5;
+int i2 = 6;
+int sum1 = i1 + i2;
+
+Console.WriteLine(sum1);
+
+long l1 = 7;
+long sum2 = l1 + i1;
+
+Console.WriteLine(sum2);
+
+double d1 = 4.7;
+double sum3 = d1 + i1;
+
+Console.WriteLine(sum3);
+
+int product1 = i1 * i2;
+long product2 = l1 * i1;
+double product3 = d1 * i1;
+
+Console.WriteLine(product1);
+Console.WriteLine(product2);
+Console.WriteLine(product3);
+
+int quotient1 = i1 / i2;
+Console.WriteLine(quotient1);
+
+d1 = 6;
+double quotient2 = d1 / i1;
+Console.WriteLine(quotient2);
