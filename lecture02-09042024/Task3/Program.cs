40918edb2bf9447028b0df5db61f59fc1e94f638
Sergey Bartasevich <sbartasevich@gmail.com>
2024-04-18 07:53:42 +0300

    add lectures/lecture02-09042024/Task3/Program.cs

---
+++ lecture02-09042024/Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿// int n = 10;
+int[] arr = { 1, 13, 23, 55, 77, 7, 15, 31, 5, 8 };
+int i = 0;
+
+while (i < arr.Length)
+{
+    if (arr[i] % 2 == 0)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    i = i + 1;
+}
