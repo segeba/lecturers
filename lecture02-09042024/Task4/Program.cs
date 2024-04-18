822ac855710e7c007f8ae22fcfb79fa14ba3b0d4
Sergey Bartasevich <sbartasevich@gmail.com>
2024-04-18 08:31:04 +0300

    modify-02 lectures/lecture02-09042024/Task4/Program.cs

--- lecture02-09042024/Task4/Program.cs
+++ lecture02-09042024/Task4/Program.cs
@@ -11,5 +11,24 @@ while (i < n)
     }
     i = i + 1;
 }
-
 Console.WriteLine(max);
+
+max = arr[0];
+for (int j = 1; j < n; j++)
+{
+    if (arr[j] > max)
+    {
+        max = arr[j];
+    }
+}
+Console.Write(max);
+
+max = arr[0];
+foreach (int e in arr)
+{
+    if (arr[e] > max)
+    {
+        max = arr[e];
+    }
+}
+Console.Write(max);
