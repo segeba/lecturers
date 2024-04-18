177b33d441893e5cfa9d82a943665d040e03d96b
Sergey Bartasevich <sbartasevich@gmail.com>
2024-04-18 07:58:17 +0300

    add lectures/lecture02-09042024/Task4/Program.cs

---
+++ lecture02-09042024/Task4/Program.cs
@@ -0,0 +1,15 @@
+﻿int n = 5;
+int[] arr = { 1, 5, 13, 8, 27 };
+int i = 0;
+int max = arr[i];
+
+while (i < n)
+{
+    if (arr[i] > max)
+    {
+        max = arr[i];
+    }
+    i = i + 1;
+}
+
+Console.WriteLine(max);
