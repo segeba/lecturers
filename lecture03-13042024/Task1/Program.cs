199209176d85deed16ba87d92894561108f805ae
Sergey Bartasevich <sbartasevich@gmail.com>
2024-04-20 19:51:28 +0300

    add Lecture03/Task1

---
+++ lecture03-13042024/Task1/Program.cs
@@ -0,0 +1,9 @@
+﻿double CalculateFormula(int a, int b, int c, int d)
+{
+    double numenator = a * b;
+    int denumenator = c + d;
+    double result = numenator / denumenator;
+    return result;
+}
+
+System.Console.WriteLine(CalculateFormula(1, 2, 3, 4));
