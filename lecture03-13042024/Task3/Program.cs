6cdb0ae75747bdf5636119b01bec36c17397a6d3
Sergey Bartasevich <sbartasevich@gmail.com>
2024-04-21 21:29:48 +0300

    add Lecture03/Task3

---
+++ lecture03-13042024/Task3/Program.cs
@@ -0,0 +1,22 @@
+﻿void ZeroEvenElemnts(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            arr[i] = 0;
+        }
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    foreach (var item in arr)
+    {
+        Console.Write($"{item} ");
+    }
+}
+
+int[] array = { 1, 2, 3, 4, 5, 6,};
+ZeroEvenElemnts(array);
+PrintArray(array);
