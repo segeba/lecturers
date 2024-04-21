374a1d6e77312faf97f0e0ae679f62ce3a0171db
Sergey Bartasevich <sbartasevich@gmail.com>
2024-04-21 21:58:57 +0300

    edit02 Lecture04/Task1

--- lecture04-16042024/Task1/Program.cs
+++ lecture04-16042024/Task1/Program.cs
@@ -1,20 +1,31 @@
-﻿int[,] matrix = new int[3, 5]; // определение нового двумерного массива - 3 строки, 5 столбцов
-
-Random rnd = new Random(); // создание объекта класса Random
-
-for (int i = 0; i < matrix.GetLength(0); i++) // matrix.GetLength(0) - количество строк двумерного массива
+﻿int [,] CreateMatrix(int rowsCount, int columnsCount) // метод для создания матрицы
 {
-    for (int j = 0; j < matrix.GetLength(1); j++) // matrix.GetLength(1) - количество столбцов двумерного массива
+    int[,] matrix = new int[rowsCount, columnsCount]; // определение нового двумерного массива - 3 строки, 5 столбцов
+
+    Random rnd = new Random(); // создание объекта класса Random
+
+    for (int i = 0; i < matrix.GetLength(0); i++) // matrix.GetLength(0) - количество строк двумерного массива
     {
-        matrix[i, j] = rnd.Next(1, 11); // заполнение массива случайными числами от 1 до 10 включительно (11 не входит в диапазон)
+        for (int j = 0; j < matrix.GetLength(1); j++) // matrix.GetLength(1) - количество столбцов двумерного массива
+        {
+            matrix[i, j] = rnd.Next(1, 11); // заполнение массива случайными числами от 1 до 10 включительно (11 не входит в диапазон)
+        }
+    }
+    return matrix;
+}
+
+void ShowMatrix(int [,] matrix) // метод вывода матрицы на экран
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i,j]} "); // вывод массива на экран поэлементно
+        }
+        Console.WriteLine(); // переход на следующую строку, построчный вывод массива
     }
 }
 
-for (int i = 0; i < matrix.GetLength(0); i++)
-{
-    for (int j = 0; j < matrix.GetLength(1); j++)
-    {
-        Console.Write($"{matrix[i,j]} "); // вывод массива на экран поэлементно, построчно
-    }
-    Console.WriteLine();
-}
+int[,] matrix = CreateMatrix(4, 5);
+
+ShowMatrix(matrix); // вывод матрицы на экран путем вызова метода ShowMatrix()
